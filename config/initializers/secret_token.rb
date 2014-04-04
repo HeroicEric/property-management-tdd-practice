08402f6eb8c86202a0c0c12f453a95285d78f0d4
Daniel Greenberg <dgreenbe777@gmail.com>
2014-04-04 13:42:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PropertyManagement::Application.config.secret_key_base = '036748d14f4a7ac6bb72e48639f0c00fa0675266118b4c9f6a33acfdf7e2f4d5b507b5feb2aa90c5f91644badfd55c2cac52bc1842d5b32a1eb6973844f55aee'
---
+++ spec/features/associate_building_recording.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+feature 'recording building information', %q{As a real estate associate
+I want to record a building
+So that I can refer back to pertinent information
+} do
+  
+  
+
+
+
+  
+end
